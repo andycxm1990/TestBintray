ec45aa118d40e65d47a4cc3e1b0fec2bfdbe84f1
andycxm1990 <andycxm1990@gamil.com>
2016-11-18 11:11:20 +0800

    first commit

---
+++ myutils/src/main/java/com/sinze/myutils/MyUtils.java
@@ -0,0 +1,16 @@
+package com.sinze.myutils;
+ 
+ 
+ /*
+ * @Description: [描述该文件做什么，必填]
+ * @version   1.0.0   [版本信息，必填]
+ * @author   User [作者，必填]
+ * @Date    2016/11/18 09:05
+ * ${tags}
+ */
+
+public class MyUtils {
+    public static void print(String msg){
+        System.out.print(msg);
+    }
+}
